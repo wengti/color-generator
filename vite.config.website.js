0427a5b5d649ff1490575bb1629c927206c8a5cd
Weng Ti <wengti@hotmail.com>
2025-12-20 00:55:31 +0800

    update static website and extension

---
+++ vite.config.website.js
@@ -0,0 +1,7 @@
+// vite.config.website.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Your normal Vite config for the website
+  base: './' // or whatever base you were using
+})
